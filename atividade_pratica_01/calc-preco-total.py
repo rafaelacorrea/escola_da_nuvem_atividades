168c47dd7efca86aaf451ad5c7efa0bb43085e04
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2025-08-22 13:18:51 -0300

    feat calculo preço total

---
+++ atividade_pratica_01/calc-preco-total.py
@@ -0,0 +1,11 @@
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+preco_total = preco_unitario * quantidade
+
+print(f"Produto: {nome_produto}")
+print(f"Preço Unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print("-" * 25)
+print(f"Preço Total: R$ {preco_total:.2f}")
